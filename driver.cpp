1fb5af97158f1754b6ea647836f4824653e300db
BrandonKi <44515121+BrandonKi@users.noreply.github.com>
2020-10-16 11:29:33 -0400

    added example code

--- driver.cpp
+++ driver.cpp
@@ -2,13 +2,13 @@
 
 using namespace pLog;
 
-
 int main(int argc, char* argv[]){
-
     init_pLog();
+    
 
-    println(fmt("test", UNDERLINE, BOLD, RED) + " " + fmt("test", BLUE));
-
-    print("test", BOLD, RED);
-
+    println("test", UNDERLINE, BOLD, RED);
+    println(fmt("I'm blue", BLUE) + " " + fmt("I'm red and bold", BRIGHT_CYAN, BOLD));
+    println("r,g,b values are also supported! They can be bold, underlined, etc.", rgb(255, 100, 0), BOLD);
+    println("to set background color pass a color to the bg() function.", bg(RED));
+    print("rgb can also be passed to bg", bg(rgb(255, 100, 0)));
 }
