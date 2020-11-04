ddb989ea780ae1ecdd09427147e86baf874e5701
BrandonKi <44515121+BrandonKi@users.noreply.github.com>
2020-11-04 12:46:08 -0500

    fixed reserved id warning

--- driver.cpp
+++ driver.cpp
@@ -5,7 +5,8 @@ using namespace pLog;
 int main(int argc, char* argv[]){
     init_pLog();
 
-    println("test", UNDERLINE, BOLD, RED);
+    println(12.5, RED, BOLD);
+    println(16, UNDERLINE, BOLD, RED);
     println(fmt("I'm blue", BLUE) + " " + fmt("I'm cyan and bold", BRIGHT_CYAN, BOLD));
     println("r,g,b values are also supported! They can be bold, underlined, etc.", rgb(255, 100, 0), BOLD);
     println("to set background color pass a color to the bg() function.", bg(RED));
